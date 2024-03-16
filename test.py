0dea4ddf17ed83f85aa1b9c150bf0a055e1b77bf
psharratt <psharratt1@gmail.com>
2024-03-16 13:36:46 +0100

    adding functions to test

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ Created on Mon Jan 29 22:15:21 2024
 from utils.repo_info import get_repo_info, fork_archive, get_org_repos
 from utils.augur_connect import augur_db_connect
 from metrics.closure_ratio import monthly_prs_closed
+from metrics.release_frequency import get_release_data
 
 engine = augur_db_connect("config.json")
 
@@ -20,8 +21,7 @@ repo_org = 'sequoia-pgp'
 org_name  = 'sequoia-pgp'
 repo_name = 'fast-forward'
 repo_id =  '150824' 
-#start_date = '202001' 
-#end_date = '202301'
+
 
 # Function & call
 repo_info = get_repo_info(engine, repo_org, repo_name)
@@ -33,33 +33,19 @@ org_repos = get_org_repos(org_name, engine)
 
 print(org_repos)
 
-<<<<<<< Updated upstream
-=======
-#repo_prs = monthly_prs_closed(repo_id, repo_name, start_date, end_date, engine)
-
-#print(repo_prs)
-
-#YYYY-MM-DD HH:MM:SS
 
 
-#150824
+### Sorting working directory
 
-#dm_group_monthly
+import os
+print(os.getcwd())
+# Define the target directory
+target_directory = "/Users/paulsharratt/Documents/Hertie/Semester 4/03 - Master's Thesis/starter-health-model_STF/"
 
-#dm_repo_monthly
+# Change the current working directory
+os.chdir(target_directory)
+
+# check the augur documentation
 
 
-
-#from metrics.closure_ratio import monthly_prs_closed
-
-#from sqlalchemy import create_engine, Column, Integer, String
-#from sqlalchemy.ext.declarative import declarative_base
-#from sqlalchemy.orm import sessionmaker
-
-#Session = sessionmaker(bind=engine)
-#session = Session()
-
-#for pr_monthDF in session.query(pr_monthDF.start_date).all():
-#    print(pr_monthDF.start_date)
-    
->>>>>>> Stashed changes
+get_release_data(repo_id, start_date, end_date, engine):
